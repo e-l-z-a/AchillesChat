4e0822ed0ca10dbb5d1378f9f24d542f3d16e69d
e-l-z-a <sandraelizabethlk@gmail.com>
2025-08-21 15:41:55 -0400

    Delete Models/ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AchillesChat.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
